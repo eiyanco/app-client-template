5ad68bf68d89f0d07320a92b2cb74556517aa3a2
ismailomodara <omodara145@gmail.com>
2025-03-17 17:16:55 +0100

    chore: successfully configured eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,29 +1,24 @@
-import pluginVue from 'eslint-plugin-vue'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import pluginVue from 'eslint-plugin-vue';
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import pluginCypress from 'eslint-plugin-cypress/flat'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import pluginCypress from 'eslint-plugin-cypress/flat';
 
-// To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
-// import { configureVueProject } from '@vue/eslint-config-typescript'
-// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
-// More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
+import stylisticTs from '@stylistic/eslint-plugin-ts';
+import parserTs from '@typescript-eslint/parser';
 
 export default defineConfigWithVueTs(
   {
     name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
+    files: ['**/*.{js,ts,vue}'],
   },
-
   {
     name: 'app/files-to-ignore',
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
   },
-
   pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
-  
+
   {
     ...pluginCypress.configs.recommended,
     files: [
@@ -32,5 +27,25 @@ export default defineConfigWithVueTs(
       'cypress/support/**/*.{js,ts,jsx,tsx}'
     ],
   },
-  skipFormatting,
-)
+
+  {
+    plugins: {
+      '@stylistic/ts': stylisticTs
+    },
+    languageOptions: {
+      "parserOptions": {
+        "parser": parserTs
+      }
+    },
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      'vue/block-lang': 'off',
+      'typescript-eslint/no-explicit-any': "off",
+      "semi": "error",
+      "prefer-const": "error",
+      '@stylistic/ts/indent': ['error', 2]
+    }
+  },
+);
--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from 'vue-router';
+import HelloWorld from './components/HelloWorld.vue';
+
+const name = "Ismail";
+console.log(name);
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/">Home </RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
+      <button>Sushi</button>
   </header>
 
   <RouterView />
